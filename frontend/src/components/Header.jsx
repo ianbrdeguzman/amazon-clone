fc9dc2fe3526b1f95ab8733d1069295b0f3be3ab
ianbrdeguzman <ianbrdeguzman@gmail.com>
2021-05-13 21:27:11 -0400

    finished header template

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+import { AiOutlineSearch, AiOutlineShoppingCart } from 'react-icons/ai';
+import logo from '../assets/Amazon_logo.svg';
+
+const Header = () => {
+    const [input, setInput] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        alert(`${input} Not yet implemented...`);
+        setInput('');
+    };
+
+    return (
+        <div className='md:flex items-center p-4 bg-primary text-white'>
+            <div className='flex flex-1'>
+                <img src={logo} alt='logo' className='w-24 md:w-32' />
+                <form
+                    className='flex-1 flex ml-4 md:mr-4 relative'
+                    onSubmit={handleSubmit}
+                >
+                    <input
+                        type='text'
+                        name='search'
+                        id='search'
+                        value={input}
+                        className='flex-1 p-2 rounded focus:outline-none focus:ring-4 focus:ring-yellow-500 text-black'
+                        onChange={(e) => setInput(e.target.value)}
+                    />
+                    <button
+                        type='submit'
+                        className='p-3 bg-yellow-400 rounded-tr rounded-br absolute top-0 right-0'
+                    >
+                        <AiOutlineSearch />
+                    </button>
+                </form>
+            </div>
+            <div className='flex mt-2 justify-between'>
+                <button className='mx-2 p-2 border border-transparent hover:border-white focus:outline-none'>
+                    <p className='text-left text-xs'>Hello, Sign in</p>
+                    <p className='text-left font-bold text-xs'>
+                        Account & Lists
+                    </p>
+                </button>
+                <button className='mx-2 p-2 border border-transparent hover:border-white focus:outline-none'>
+                    <p className='text-left text-xs'>Returns</p>
+                    <p className='text-left font-bold text-xs'>& Orders</p>
+                </button>
+                <button className='flex items-center mx-2 pl-2 pr-4 border border-transparent hover:border-white relative focus:outline-none'>
+                    <AiOutlineShoppingCart size={28} />
+                    <span className='absolute right-0 top-0 bg-yellow-500 px-2 rounded-full'>
+                        0
+                    </span>
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,10 +2,16 @@ module.exports = {
     purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
     darkMode: false, // or 'media' or 'class'
     theme: {
+        backgroundColor: (theme) => ({
+            ...theme('colors'),
+            primary: '#131921',
+        }),
         extend: {},
     },
     variants: {
-        extend: {},
+        extend: {
+            borderWidth: ['hover'],
+        },
     },
     plugins: [],
 };
