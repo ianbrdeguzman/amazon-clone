9dfe0f03f4afa1d843e3a541203754a7cde2705a
ianbrdeguzman <ianbrdeguzman@gmail.com>
2021-05-20 09:58:09 -0400

    created get order api

--- backend/routers/orderRouter.js
+++ backend/routers/orderRouter.js
@@ -31,4 +31,16 @@ orderRouter.post(
     })
 );
 
+orderRouter.get(
+    '/:id',
+    expressAsyncHandler(async (req, res) => {
+        const order = await Order.findById(req.params.id);
+        if (order) {
+            res.send(order);
+        } else {
+            res.status(404).send({ message: 'Order not found.' });
+        }
+    })
+);
+
 export default orderRouter;
