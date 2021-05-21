24bffb2e828a1c772a966ada66e97af868f524f4
ianbrdeguzman <ianbrdeguzman@gmail.com>
2021-05-20 20:44:04 -0400

    created payment api backend

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -24,6 +24,12 @@ const orderSchema = new mongoose.Schema(
             phone: { type: String, required: true },
         },
         paymentMethod: { type: String, required: true },
+        paymentDetails: {
+            id: { type: String },
+            status: { type: String },
+            update_time: { type: String },
+            email_address: { type: String },
+        },
         itemPrice: { type: Number, required: true },
         shippingPrice: { type: Number, required: true },
         taxPrice: { type: Number, required: true },
